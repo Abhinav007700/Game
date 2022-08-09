2e56102035d042ccf3ca172d5749b5b278d928b5
Boora Hruday Abhinav Manohar <105707241+Abhinav007700@users.noreply.github.com>
2022-08-09 22:45:00 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,182 +1,157 @@
-document.addEventListener("keypress", function(event) {
-	const element = document.getElementById("text");
-    element.innerHTML="";
-    randomNumber();
-});
-var p1b1=0;
-var p1b2=0;
-var p1b3=0;
-var p1b4=0;
-var p1b5=0;
-var p2b1=0;
-var p2b2=0;
-var p2b3=0;
-var p2b4=0;
-var p2b5=0;
+document.querySelector("button").onclick=doit;
+document.getElementById('0').onclick=tapped;
+document.getElementById('1').onclick=tapped;
+document.getElementById('2').onclick=tapped;
+document.getElementById('3').onclick=tapped;
+document.getElementById('4').onclick=tapped;
+document.getElementById('5').onclick=tapped;
+document.getElementById('6').onclick=tapped;
+document.getElementById('7').onclick=tapped;
+document.getElementById('8').onclick=tapped;
+document.getElementById('9').onclick=tapped;
+document.getElementById('10').onclick=tapped;
+document.getElementById('11').onclick=tapped;
+document.getElementById('12').onclick=tapped;
+document.getElementById('13').onclick=tapped;
+document.getElementById('14').onclick=tapped;
+document.getElementById('15').onclick=tapped;
+var b=null;
+var p=[];
+var q=[];
+var x=1;
+disable_button();
+function tapped(){
+    if (this.id !=undefined){
+        b=String(this.id);
+        if(q.includes(b)){
+            console.log('same');
+        }
+        else{
+            q.push(b);
+            blinkColor1(b);
+            compare(b);
+        }
+    }
+}
+function doit(){
+    document.getElementById("demo").innerHTML = " ";
+    next();
+}
+function next(){
+        disable_button();
+        var j=randomNumber();
+        unique_element(j);
+        setTimeout(function(){
+            for(y of p){
+                var e=p.indexOf(y);
+                delay(e);
+            }
+        }, 1000);
+        setTimeout(function(){
+            document.getElementById("demo1").innerHTML = "Your turn!";
+        }, 1500+500*(p.length));
+        setTimeout(function(){
+            document.getElementById("demo1").innerHTML = " ";
+            enable_button();
+        }, 2000+500*(p.length));
+}
+function delay(e){
+    setTimeout(function(){
+        blinkColor(p[e]);
+    },500*(e+1));
+}
+function unique_element(j){
+    if (p.includes(String(j))){
+        var w=randomNumber();
+        unique_element(w);
+    }
+    else{
+        p.push(String(j));
+        console.log(p);
+    }
+}
+function disable_button(){
+    document.getElementById('0').disabled = true;
+    document.getElementById('1').disabled = true;
+    document.getElementById('2').disabled = true;
+    document.getElementById('3').disabled = true;
+    document.getElementById('4').disabled = true;
+    document.getElementById('5').disabled = true;
+    document.getElementById('6').disabled = true;
+    document.getElementById('7').disabled = true;
+    document.getElementById('8').disabled = true;
+    document.getElementById('9').disabled = true;
+    document.getElementById('10').disabled = true;
+    document.getElementById('11').disabled = true;
+    document.getElementById('12').disabled = true;
+    document.getElementById('13').disabled = true;
+    document.getElementById('14').disabled = true;
+    document.getElementById('15').disabled = true;
+}
+function enable_button(){
+    document.getElementById('0').disabled = false;
+    document.getElementById('1').disabled = false;
+    document.getElementById('2').disabled = false;
+    document.getElementById('3').disabled = false;
+    document.getElementById('4').disabled = false;
+    document.getElementById('5').disabled = false;
+    document.getElementById('6').disabled = false;
+    document.getElementById('7').disabled = false;
+    document.getElementById('8').disabled = false;
+    document.getElementById('9').disabled = false;
+    document.getElementById('10').disabled = false;
+    document.getElementById('11').disabled = false;
+    document.getElementById('12').disabled = false;
+    document.getElementById('13').disabled = false;
+    document.getElementById('14').disabled = false;
+    document.getElementById('15').disabled = false;
+}
+function finish(){
+    alert("Congratulations, Game successfully completed");
+    document.getElementById("demo").innerHTML = "Score: "+String((x-1)*25);
+    disable_button();
+    p=[];
+    x=1;
+}
+function compare(b){
+        if (p.includes(b)){
+            if(p.length == q.length){
+                if (p.length==16){
+                    x+=1;
+                    finish();
+                }
+                else{
+                    next();
+                    x+=1;
+                    q=[];
+                }
+                
+            }
+        }
+        else{
+            end();
+        }
+}
+function end(){
+    document.getElementById("demo").innerHTML = "Score: "+String((x-1)*25);
+    alert('Game over');
+    disable_button();
+    p=[];
+    q=[];
+    x=1;
+}
 function randomNumber(){
-    let p1bn= 1 + Math.floor(Math.random() * 5);
-    let p2bn= 1 + Math.floor(Math.random() * 5);
-    if (p1bn==1){
-        document.getElementById("01").innerHTML = "Box Number(Player 1): 1";
-        if (p1b1<=4){
-            p1b1+=1;
-            figureMaking('5',p1b1);
-        }
-        else{
-            p1b1+=1;
-        }
-    }
-    if (p1bn==2){
-        document.getElementById("01").innerHTML = "Box Number(Player 1): 2";
-        if (p1b2<=4){
-            p1b2+=1;
-            figureMaking('8',p1b2);
-        }
-        else{
-            p1b2+=1;
-        }
-    }
-    if (p1bn==3){
-        document.getElementById("01").innerHTML = "Box Number(Player 1): 3";
-        if (p1b3<=4){
-            p1b3+=1;
-            figureMaking('11',p1b3);
-        }
-        else{
-            p1b3+=1;
-        }
-    }
-    if (p1bn==4){
-        document.getElementById("01").innerHTML = "Box Number(Player 1): 4";
-        if (p1b4<=4){
-            p1b4+=1;
-            figureMaking('14',p1b4);
-        }
-        else{
-            p1b4+=1;
-        }
-    }
-    if (p1bn==5){
-        document.getElementById("01").innerHTML = "Box Number(Player 1): 5";
-        if (p1b5<=4){
-            p1b5+=1;
-            figureMaking('17',p1b5);
-        }
-        else{
-            p1b5+=1;
-        }
-    }
-
-    if (p2bn==1){
-        document.getElementById("02").innerHTML = "Box Number(Player 2): 1";
-        if (p2b1<=4){
-            p2b1+=1;
-            figureMaking('6',p2b1);
-        }
-        else{
-            p2b1+=1;
-        }
-    }
-    if (p2bn==2){
-        document.getElementById("02").innerHTML = "Box Number(Player 2): 2";
-        if (p2b2<=4){
-            p2b2+=1;
-            figureMaking('9',p2b2);
-        }
-        else{
-            p2b2+=1;
-        }
-    }
-    if (p2bn==3){
-        document.getElementById("02").innerHTML = "Box Number(Player 2): 3";
-        if (p2b3<=4){
-            p2b3+=1;
-            figureMaking('12',p2b3);
-        }
-        else{
-            p2b3+=1;
-        }
-    }
-    if (p2bn==4){
-        document.getElementById("02").innerHTML = "Box Number(Player 2): 4";
-        if (p2b4<=4){
-            p2b4+=1;
-            figureMaking('15',p2b4);
-        }
-        else{
-            p2b4+=1;
-        }
-    }
-    if (p2bn==5){
-        document.getElementById("02").innerHTML = "Box Number(Player 2): 5";
-        if (p2b5<=4){
-            p2b5+=1;
-            figureMaking('18',p2b5);
-        }
-        else{
-            p2b5+=1;
-        }
-    }
-    const delay= setTimeout(function(){
-        if (p1b1>=4 && p1b2>=4 && p1b3>=4 && p1b4>=4 && p1b5>=4){
-            alert("Player 1 won the game");
-            clearTimeout(myTimeout);
-            clearTimeout(delay);
-            gameOver();
-        }
-    
-        if (p2b1>=4 && p2b2>=4 && p2b3>=4 && p2b4>=4 && p2b5>=4){
-            alert("Player 2 won the game");
-            clearTimeout(myTimeout);
-            clearTimeout(delay);
-            gameOver();
-        }
-    },100);
-
-    const myTimeout = setTimeout(randomNumber,1500);
+    return Math.floor((Math.random() * 16));
 }
-function figureMaking(id, number){
-    document.getElementById(id).innerHTML = "";
-    let elem = document.createElement("img");
-    elem.setAttribute("height", "100");
-    elem.setAttribute("width", "200");
-    elem.setAttribute("alt", "Figure");
-    if (number==1){
-        elem.setAttribute("src", "a1.png");
-    }
-    if (number==2){
-        elem.setAttribute("src", "a2.png");
-    }
-    if (number==3){
-        elem.setAttribute("src", "a3.png");
-    }
-    if (number>=4){
-        elem.setAttribute("src", "a4.png");
-    }
-    document.getElementById(id).appendChild(elem);
+function blinkColor(color) {
+    let colorButton = document.getElementById(color);
+    console.log(color);
+    colorButton.style.background = "blue";  
+    setTimeout(() => {colorButton.style.background = "Black"}, 500);
 }
-function gameOver(){
-    p1b1=0;
-    p1b2=0;
-    p1b3=0;
-    p1b4=0;
-    p1b5=0;
-    p2b1=0;
-    p2b2=0;
-    p2b3=0;
-    p2b4=0;
-    p2b5=0;
-    document.getElementById('5').innerHTML = "";
-    document.getElementById('6').innerHTML = "";
-    document.getElementById('8').innerHTML = "";
-    document.getElementById('9').innerHTML = "";
-    document.getElementById('11').innerHTML = "";
-    document.getElementById('12').innerHTML = "";
-    document.getElementById('14').innerHTML = "";
-    document.getElementById('15').innerHTML = "";
-    document.getElementById('17').innerHTML = "";
-    document.getElementById('18').innerHTML = "";
-    document.getElementById("01").innerHTML = "Box Number(Player 1): --";
-    document.getElementById("02").innerHTML = "Box Number(Player 2): --";
-    document.getElementById('text').innerHTML = "Press any key to start";
+function blinkColor1(color) {
+    let colorButton = document.getElementById(color);
+    console.log(color);
+    colorButton.style.background = "green";  
+    setTimeout(() => {colorButton.style.background = "Black"}, 250);
 }
